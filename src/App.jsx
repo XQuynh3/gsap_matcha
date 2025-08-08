738354d8eef3d097f77238779f69a4e3a7df57dd
XQuynh3 <xuanquynh3824@gmail.com>
2025-08-08 16:25:08 +0700

    Implement scroll

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ const App = () => {
         <main>
             <Navbar />
             <Hero />
-            <div className="h-dvh bg-black"/>
         </main>
     );
 };
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,16 @@
 import gsap from "gsap";
 import { useGSAP} from "@gsap/react";
 import { SplitText} from "gsap/all";
+import {useMediaQuery} from "react-responsive";
+import {useRef} from "react";
 
 
 const Hero = () => {
+    const videoRef = useRef();
+
+    const isMobile = useMediaQuery( {maxWidth: 767 });
+
+
     useGSAP(() => {
         const heroSplit = new SplitText('.title', { type: 'chars, words'});
         const paragraphSplit = new SplitText('.subtitle', { type: 'lines'});
@@ -33,13 +40,36 @@ const Hero = () => {
                 end: 'bottom top',
                 scrub: true,
             }
-        });
+        })
+
+        .to('.right-leaf', { y: 200}, 0)
+        .to('.left-leaf', { y: -200 }, 0)
+
+        const startValue = isMobile ? 'top 50%' : 'center 60%';
+        const endValue = isMobile ? '120% top' : 'bottom top';
+
+        const tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: 'video',
+                start: startValue,
+                end: endValue,
+                scrub: true,
+                pin: true,
+            }
+        })
+
+        videoRef.current.onloadedmetadata = () => {
+            tl.to(videoRef.current, {
+                currentTime: videoRef.current.duration,
+            })
+        }
 
     }, []);
 
 
 
     return (
+       <>
         <section id = "hero" className="noisy">
             <h1 className="title">MIMOSA</h1>
 
@@ -66,16 +96,25 @@ const Hero = () => {
                             Every mocktail on our menu is a vibrant blend of fresh ingredients, imaginative flavors, and pure refreshment — designed to elevate your spirits without the spirits.
                         </p>
                         <a href = "#mocktails">View Mocktails</a>
-
                     </div>
-
-
                 </div>
-
             </div>
+        </section>
 
 
-        </ section>
+        <div className="video absolute inset-0">
+            <video
+                ref={videoRef}
+                src="/videos/output.mp4"
+                muted
+                playsInline
+                preload="auto"
+
+            />
+
+        </div>
+       </>
+
     );
 };
 
